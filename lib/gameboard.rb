7fff1cdbd588fb916968f416be08d67fb0d1f5fb
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-08-01 14:27:46 -0500

    add gameboard#mark tests

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
 
+# Contains internal state of tic-tac-toe grid and answers questions about the grid's state
 class Gameboard
   def initialize(size)
     @board = Array.new(size * size)
@@ -11,6 +13,11 @@ class Gameboard
   def mark(cell, marker)
     index = cell_to_index(cell)
     @board[index] = marker
+  end
+
+  def space_free?(cell)
+    index = cell_to_index(cell)
+    @board[index].nil?
   end
 
   private
--- spec/gameboard_spec.rb
+++ spec/gameboard_spec.rb
@@ -1,75 +1,82 @@
-require_relative '../lib/gameboard.rb'
+require_relative '../lib/gameboard'
 
 describe Gameboard do
-  # This is a query message.  I want to change nothing but return something to verify state.
   describe '#open_spaces' do
     context 'when game is 3x3' do
       subject(:nine_space_game) { described_class.new(3) }
 
-      # This test makes sense because #open_spaces can be tested in isolation.
       context 'when no spaces are marked' do
         it 'has 9 open spaces' do
           open_spaces = nine_space_game.open_spaces
           expect(open_spaces.count).to eq(9)
         end
       end
+    end
 
-      # This is where it gets fuzzy.  I want to test #open_spaces 
-      # under other conditions that requires a space to be marked.  
-      # In order to do so I would like to call #mark.
+    context 'when game is 10x10' do
+      subject(:hundred_space_game) { described_class.new(10) }
 
-      # 1. Should this be a test on #mark because I send a command 
-      #    (#mark) and want to then verify state with a query (#open_spaces)?
-      #    Since #open_spaces is a query I'm not sure that I should be 
-      #    changing state to test it here.
-      #
-      #    If I go this route it seems that #mark and #open_spaces 
-      #    are necessary to test each other in either case.
-      #
-      # 2. One option to test #open_spaces in isolation could be to 
-      #    allow Gameboard's #initialize to accept an array so I can create
-      #    a gameboard with preset spots like this:
-      #    ex: predefined_board  = Gameboard.new([ X , nil, nil,
-      #                                           nil, nil, nil,
-      #                                           nil, nil, nil])
-      #
-      #    The problem then becomes what if an array is passed that is 
-      #    not a square.  Do I add complexity to Gameboard's #initialize
-      #    to check for this solely for the ability to create 
-      #    Gameboards with predefined spaces for testing? I don't think
-      #    there is a need for this behavior outside of this style of testing.
-      #
-      # 3. Ignore #mark while testing #open_spaces and mark a space by 
-      #    accessing Gameboard's internal @spaces array. I could do this
-      #    with instance_variable_get(:@spaces).  Another similar option is to
-      #    expose @spaces through a public method.  Both of these seem like 
-      #    the worst ideas since @spaces should be private and not mutable 
-      #    outside of #mark.
-      # 
-      # 4. Am I overlooking a better approach?
-      # 5. Is the current approach good or good enough?
+      context 'when no spaces are marked' do
+        it 'has 100 open spaces' do
+          open_spaces = hundred_space_game.open_spaces
+          expect(open_spaces.count).to eq(100)
+        end
+      end
+    end
+  end
 
-      context 'when space 0 is marked' do
+  describe '#mark' do
+    context 'when game is 3x3' do
+      subject(:marked_game) { described_class.new(3) }
+
+      context 'when space 1 is marked' do
         before do
-          nine_space_game.mark(0, 'X')
+          marked_game.mark(1, 'X')
         end
 
         it 'has 8 open spaces' do
-          open_spaces = nine_space_game.open_spaces
+          open_spaces = marked_game.open_spaces
 
           expect(open_spaces.count).to eq(8)
         end
 
-        it 'does not include 0 as open' do
-          open_spaces = nine_space_game.open_spaces
+        it 'does not include 1 as open' do
+          open_spaces = marked_game.open_spaces
 
-          expect(open_spaces).not_to include(0)
+          expect(open_spaces).not_to include(1)
         end
 
-        it 'includes spaces 1-8 as open' do
-          open_spaces = nine_space_game.open_spaces
-          expected_open_spaces = (1..8).to_a
-          
+        it 'includes spaces 2-9 as open' do
+          open_spaces = marked_game.open_spaces
+          expected_open_spaces = (2..9).to_a
+
+          expect(open_spaces).to eq(expected_open_spaces)
+        end
+      end
+
+      context 'when spaces 1, 5, and 9 are marked' do
+        before do
+          marked_game.mark(1, 'X')
+          marked_game.mark(5, 'X')
+          marked_game.mark(9, 'X')
+        end
+
+        it 'has 6 open spaces' do
+          open_spaces = marked_game.open_spaces
+
+          expect(open_spaces.count).to eq(6)
+        end
+
+        it 'does not include 1, 5, and 9 as open' do
+          open_spaces = marked_game.open_spaces
+
+          expect(open_spaces).not_to include(1, 5, 9)
+        end
+
+        it 'includes spaces 2-9 as open' do
+          open_spaces = marked_game.open_spaces
+          expected_open_spaces = (1..9).to_a - [1, 5, 9]
+
           expect(open_spaces).to eq(expected_open_spaces)
         end
       end
