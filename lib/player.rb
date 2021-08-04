dc52fa814a89958068400df8e5cac243c06f0143
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-08-03 20:24:12 -0500

    player stores own color code

--- lib/player.rb
+++ lib/player.rb
@@ -2,10 +2,14 @@
 
 # Parent class for all player types
 class Player
-  attr_reader :marker, :color_code
+  attr_reader :color_code
 
   def initialize(marker, color_code = nil)
     @marker = marker
     @color_code = color_code
+  end
+
+  def marker
+    @color_code.nil? ? @marker : "\e[#{@color_code}#{@marker}\e[0m"
   end
 end
